f0524961e614a23f0ebef0063b8003b6b9f3c99d
Ben Watkins <hi@bwatkins.dev>
2023-11-25 11:36:53 -0600

    sending health checks with explicit runner/job approach

--- src/cli.ts
+++ src/cli.ts
@@ -1,8 +1,9 @@
 import { readFile } from 'node:fs/promises';
 import { parseDocument } from 'yaml';
 import * as c from 'ansi-colors';
-import { createJob, createRunner, yoloSchema } from './health';
+import { HealthCheckConfig, createHealthCheckJob, yoloSchema } from './health';
 import { z } from 'zod';
+import { createRunner } from './runner';
 
 const main = async (args = process.argv.slice(2)) => {
   if (args.length < 1) {
@@ -16,29 +17,61 @@ const main = async (args = process.argv.slice(2)) => {
   }
 
   // default error provides plenty of info especially considering our audience is developers
-  // const manifest = await readFile(args[0], 'utf8');
-  // const stuff = parseDocument(manifest).toJSON();
-  // console.log(stuff);
-  // const abc = z.array(yoloSchema).parse(stuff);
-  // console.log(abc);
+  const manifest = await readFile(args[0], 'utf8');
+  const stuff = parseDocument(manifest).toJSON();
+  const config = z.array(yoloSchema).parse(stuff);
 
   const ac = new AbortController();
 
-  const { run, locks } = createRunner(15_000, [
-    createJob({
-      name: 'testerino',
-      url: 'https://bwatkins.dev',
-    }),
-    createJob({
-      name: 'testerino 2',
-      url: 'https://bwatkins.dev',
-      method: 'HEAD',
-    }),
-  ]);
+  const { run, locks, bus } = createRunner<HealthCheckConfig>(
+    15_000,
+    config.map(createHealthCheckJob)
+  );
 
   process.on('SIGINT', () => {
     console.log(`${c.bold(c.yellow('warn'))} cancelling ${locks.size} jobs..`);
     ac.abort('user asked to stop monitoring');
+  });
+
+  bus.on('job:end', (data) => {
+    console.log(data);
+  });
+  // bus.on('job:success', ({ id }) => {
+  //   console.log(`${c.bold(c.gray('[runner]'))} job '${id}' succeeded`);
+  // });
+  // bus.on('job:error', ({ id, error }) => {
+  //   console.log(`${c.bold(c.gray('[runner]'))} job '${id}' failed`, error);
+  // });
+
+  const tracking = new Map<string, { total: number; ok: number }>();
+  bus.on('batch:end', ({ jobs }) => {
+    for (const job of jobs) {
+      if (job.outcome === 'error') {
+        continue;
+      }
+
+      const { hostname } = new URL(job.config.url);
+      const data = tracking.get(hostname) ?? {
+        total: 0,
+        ok: 0,
+      };
+
+      const ok = job.checks.every((c) => c.passed);
+      if (ok) {
+        data.ok += 1;
+      }
+      data.total += 1;
+
+      tracking.set(hostname, data);
+    }
+
+    tracking.forEach((data, hostname) => {
+      console.log(
+        `${hostname} has ${Math.round(
+          100 * (data.ok / data.total)
+        )}% availability percentage`
+      );
+    });
   });
 
   try {
--- src/health.ts
+++ src/health.ts
@@ -1,8 +1,7 @@
 import { randomUUID } from 'node:crypto';
-import { setInterval, setTimeout } from 'node:timers/promises';
+import { setTimeout } from 'node:timers/promises';
 import { z } from 'zod';
-import * as c from 'ansi-colors';
-import EventEmitter from 'node:events';
+import type { Job } from './runner';
 
 export const httpMethodSchema = z.enum([
   'GET',
@@ -25,11 +24,7 @@ export const yoloSchema = z.object({
 });
 export type HealthCheckConfig = z.infer<typeof yoloSchema>;
 
-// P — The HTTP response code is 2xx (any 200–299 response code) and the response
-// latency is less than 500 ms.
-// DOWN — The endpoint is not UP.
-
-type Check = {
+export type Check = {
   label: string;
   /**
    * Predicate {@code true} when condition, described by label, passes
@@ -41,14 +36,7 @@ type Check = {
   }) => boolean;
 };
 
-type Job = {
-  id: string;
-  run: (
-    signal: AbortSignal
-  ) => Promise<Array<{ label: string; passed: boolean }>>;
-};
-
-const conditions: Check[] = [
+const checks: Check[] = [
   {
     label: '2xx status code',
     test: ({ response }) => response.ok,
@@ -57,14 +45,20 @@ const conditions: Check[] = [
     label: 'latency <500ms',
     test: ({ startedAt, endedAt }) => endedAt - startedAt < 500,
   },
+  // add more...
+  // maybe response body checks? `$.status = 'ok'` sorta thing
 ];
 
-export const createJob = (config: HealthCheckConfig): Job => {
+// job interface is bleeding into this file but we're just gonna live with that for sake of simplicity
+export const createHealthCheckJob = (
+  config: HealthCheckConfig
+): Job<HealthCheckConfig> => {
   return {
     id: randomUUID(),
+    config,
     run: async (signal) => {
       const t = Math.random() * 18000;
-      console.log('job running', t);
+      // console.log('job running', t);
       await setTimeout(t, undefined, { signal });
       const headers = new Headers(config.headers);
       if (config.body) {
@@ -88,83 +82,11 @@ export const createJob = (config: HealthCheckConfig): Job => {
       }
       const endedAt = Date.now();
 
-      return conditions.map((c) => ({
+      return checks.map((c) => ({
         label: c.label,
         passed: c.test({ startedAt, endedAt, response }),
         ts: endedAt,
       }));
-    },
-  };
-};
-
-export const createRunner = (intervalMs: number, jobs: Job[]) => {
-  const locks = new Map<string, boolean>();
-  const bus = new EventEmitter();
-
-  console.log(`${c.bold(c.gray('[runner]'))} scheduling ${jobs.length} job(s)`);
-
-  const run = async (signal: AbortSignal) => {
-    for (const job of jobs) {
-      if (locks.has(job.id)) {
-        continue;
-      }
-
-      locks.set(job.id, true);
-
-      // Let jobs dangle in background
-      const ac = new AbortController();
-
-      const timeoutId = global.setTimeout(() => {
-        ac.abort('job timed out');
-      }, 10_000);
-
-      function onRunnerStop() {
-        console.log(
-          `${c.bold(c.gray('[runner]'))} cancelling run of job '${job.id}'`
-        );
-        clearTimeout(timeoutId);
-        ac.abort(signal.reason);
-      }
-
-      signal.addEventListener('abort', onRunnerStop);
-
-      // console.log(`${c.bold(c.gray('[runner]'))} start run of job '${job.id}'`);
-      bus.emit(`job:begin`, { id: job.id });
-      job
-        .run(ac.signal)
-        .then(() => {
-          bus.emit(`job:end`, { id: job.id });
-          console.log(
-            `${c.bold(c.gray('[runner]'))} completed run of job '${job.id}'`
-          );
-        })
-        .catch((error) => {
-          bus.emit(`job:end`, { id: job.id });
-          console.log(
-            `${c.bold(c.gray('[runner]'))} failed run of job '${job.id}'`,
-            error
-          );
-        })
-        .finally(() => {
-          clearTimeout(timeoutId);
-          signal.removeEventListener('abort', onRunnerStop);
-          locks.delete(job.id);
-        });
-    }
-  };
-
-  return {
-    locks,
-    bus,
-    run: async (signal: AbortSignal) => {
-      // kick a run off right away -- `setInterval` waits for `intervalMs` before running
-      await run(signal);
-
-      for await (const _ of setInterval(intervalMs, undefined, {
-        signal,
-      })) {
-        await run(signal);
-      }
     },
   };
 };
